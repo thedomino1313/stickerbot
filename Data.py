b1971fe7ca53f91393d8bb0d537f40270bff6816
thedomino1313 <dominic.c.beyer@gmail.com>
2022-03-26 23:19:03 -0400

    Initial files

---
+++ Data.py
@@ -0,0 +1,277 @@
+import json
+from random import choice
+from time import time
+
+jinfo = "data.json"
+jteams = "teams.json"
+jbase = "baseteam.json"
+messages = json.load(open("messages.json"))
+
+def getData():
+    return json.load(open(jinfo))
+
+def getTeams():
+    return json.load(open(jteams))
+
+def getBase():
+    return json.load(open(jbase))
+
+def printTeams():
+    teams = getTeams()
+    s = ''
+    for team in teams:
+        s += str(team) + "\n"
+    return s
+
+def resetTeams():
+    teams = getTeams()
+    for team in teams:
+        teams[team] = getBase()
+    jfile = open(jteams, 'w')
+    json.dump(teams, jfile)
+    jfile.close()
+    return "Teams refreshed!"
+
+def addStickerToDatabase(name, points, hint):
+    info = getData()
+    if name not in info:
+        info[name] = {"points": points, "found": False, "hint": hint}
+        jfile = open(jinfo, 'w')
+        json.dump(info, jfile)
+        jfile.close()
+        return "Sticker added to database."
+    return "Sticker already exists."
+
+def removeStickerFromDatabase(name):
+    info = getData()
+    if name in info:
+        info.pop(name)
+        jfile = open(jinfo, 'w')
+        json.dump(info, jfile)
+        jfile.close()
+        return "Sticker removed from database."
+    return "Sticker does not exist."
+
+def updateHint(sticker, hint):
+    info = getData()
+    if sticker in info:
+        info[sticker]["hint"] = hint
+        jfile = open(jinfo, 'w')
+        json.dump(info, jfile)
+        jfile.close()
+        return "Sticker hint updated."
+    return "Sticker does not exist."
+
+def updateStickerName(sticker, name):
+    info = getData()
+    if sticker in info:
+        info[name] = info[sticker]
+        info.pop(sticker)
+        jfile = open(jinfo, 'w')
+        json.dump(info, jfile)
+        jfile.close()
+        return "Sticker name updated."
+    return "Sticker does not exist."
+
+def updateCost(sticker, points):
+    info = getData()
+    if sticker in info:
+        info[sticker]["points"] = points
+        jfile = open(jinfo, 'w')
+        json.dump(info, jfile)
+        jfile.close()
+        return "Sticker cost updated."
+    return "Sticker does not exist."
+
+def updateTeamName(team, name):
+    teams = getTeams()
+    if team in teams:
+        teams[name] = teams[team]
+        teams.pop(team)
+        jfile = open(jteams, 'w')
+        json.dump(teams, jfile)
+        jfile.close()
+        return "Team name updated."
+    return "Team does not exist."
+
+def addSticker(teamName, stickerName, stickerCode):
+    i = 0
+    #First get data
+    info = getData()
+    teams = getTeams()
+    full_name = stickerName + stickerCode
+    #Find index for sticker
+    if full_name not in info:
+        return "This sticker does not exist, please check your code."
+    
+    if full_name in teams[teamName]["stickers"]:
+        return "You have already found this sticker, you're too smart!"
+    else:
+        s = "Sticker recorded!"
+        teams[teamName]["stickers"].append(full_name)
+        teams[teamName]["score"] += int(info[full_name]["points"])
+        teams[teamName]["count"] += 1
+        teams[teamName]["lastnewhint"] = 0
+        if any(full_name == x[0] for x in teams[teamName]["hint"]):
+            s += "\nYou have cleared the standard hint: {}".format(teams[teamName]["hint"].pop(teams[teamName]["hint"].index([full_name, getData()[full_name]["hint"]]))[1])
+        if int(info[full_name]["points"]) > 1 and len(teams[teamName]["ghint"]) > 0:
+            teams[teamName]["ghintcomplete"] = True 
+            s += "\nYou have cleared the golden hint: {}".format(teams[teamName]["ghint"][1])
+            teams[teamName]["ghint"] = []
+        if len(teams[teamName]["stickers"]) == len(info):
+            s += "\nYou have found all of the stickers!"
+        jfile = open(jteams, 'w')
+        json.dump(teams, jfile)
+        jfile.close()
+        return s
+    
+def addTeam(teamName):
+    teamName = teamName.replace(" ", "", teamName.count(" "))
+    teams = getTeams()
+    if teamName not in teams:
+        teams[teamName] = getBase()
+        jfile = open(jteams, 'w')
+        json.dump(teams, jfile)
+        jfile.close()
+        return
+
+def removeTeam(teamName):
+    teams = getTeams()
+    if teamName in teams:
+        teams.pop(teamName)
+        jfile = open(jteams, 'w')
+        json.dump(teams, jfile)
+        jfile.close()
+        return "Team removed!"
+    return "Team already does not exist."
+
+
+def getScore(teamName):
+    return getTeams()[teamName]["score"]
+
+def getCount(teamName):
+    return getTeams()[teamName]["count"]
+
+def printScoreAndCount(teamName):
+    if teamName not in getTeams():
+        return messages["validMessage"]
+    return "Score is: " + str(getScore(teamName)) + "\nYou have found this many stickers: " + str(getCount(teamName))
+
+def getHint(teamName):
+    team = getTeams()[teamName]
+    data = getData()
+    hints = []
+    ghints = []
+    for sticker in data:
+        if sticker not in team["stickers"]:
+            if data[sticker]["points"] == '1':
+                hints.append((sticker, data[sticker]["hint"]))
+            else:
+                ghints.append((sticker, data[sticker]["hint"]))
+    newh = False
+    if time() - team["lastnewhint"] >= 1800 and len(hints) > 0:
+        team["hint"].append(choice(hints))
+        team["lastnewhint"] = time()
+        newh = True
+    newghint = False
+    if team["count"] >= 0 and not team["ghintcomplete"] and len(team["ghint"]) == 0 and len(ghints) > 0:
+        team["ghint"] = choice(ghints)
+        newghint = True
+    output = ''
+    if len(hints) == 0:
+        output += "You have no more hints to unlock.\n"
+    elif newh:
+        output += "You have a new hint!\n"
+    else: 
+        output += "You have {:.2f} more minutes until you can receive another standard hint.\n".format(30-((time()-team["lastnewhint"])/60))
+    if len(team["hint"]) == 0:
+        output += "You currently have no available standard hints.\n"
+    elif len(team["hint"]) == 1:
+        output += "You currently have one available standard hint:\n"
+    else:
+        output += "You have {} available standard hints:\n".format(len(team["hint"]))
+    for hint in team["hint"]:
+        output += hint[1] + "\n"
+    
+    if newghint:
+        output += "You have just unlocked a golden hint! The hint is:\n{}".format(team["ghint"][1])
+    elif team["count"] >= 0 and not team["ghintcomplete"] and len(team["ghint"]) == 0 and len(ghints) == 0:
+        output += "You found all of the golden stickers without a golden hint, congratulations!"
+    elif len(team["ghint"]) == 0 and team["count"] < 20:
+         output += "You will unlock a golden sticker hint after you find {} more stickers.\n".format(20-team["count"])
+    elif not team["ghintcomplete"]:
+        output += "Your golden sticker hint is:\n{}".format(team["ghint"][1])
+    else:
+        output += "You have already solved your golden hint, congratulations!"
+    teams = getTeams()
+    teams[teamName] = team
+    jfile = open(jteams, 'w')
+    json.dump(teams, jfile)
+    jfile.close()
+    return output
+        
+    
+
+#Basically just updating string lengths
+def updateFiles(oldFile, newFile, oldTeams, newTeams):
+    oldData =  open(oldFile, "w+")
+    newData = open(newFile, "r")
+
+    for line in newData:
+        oldData.write(line + "\n")
+
+    oldT =  open(oldTeams, "w+")
+    newT = open(newTeams, "r")
+
+    for line in newT:
+        oldT.write(line + "\n")
+
+    oldData.close()
+    newData.close()
+    oldT.close()
+    newT.close()
+
+def scoreBoard():
+    teams = getTeams()
+
+    scoreDict = dict()
+
+    maxLen = 0
+
+    for team in teams:
+        if len(team) > maxLen:
+            maxLen = len(team)
+
+        scoreDict[team] = getScore(team), getCount(team)
+    
+
+    order = (sorted(scoreDict.items(), key = lambda x:x[1], reverse=True))
+
+
+    file = open("data.txt", "w+", encoding="utf-8")
+    file.write("╔" + "═"*(maxLen +2) + "╦" + "═"*(8) +"╦" + "═"*(8) + "╗" + "\n")
+    file.write("║" + "Name".center(maxLen + 2, " ") + "║" +  "Score".center(8, " ") + "║" +"Count".center(8, " ") + "║" +"\n")
+    file.write
+
+    file.write("╠"+ "═"*(maxLen+2) + "╬" + "═"*(8) +  "╬" + "═"*(8)+ "║" + "\n")
+
+    i = 0
+    for t in order:
+        file.write("║" + str(t[0]).center(maxLen + 2, " ") + "║" + str(t[1][0]).center(8, " ") + "║" + str(t[1][1]).center(8, " ") + "║" + "\n")
+        
+        if i!= len(order) - 1:
+            file.write("╠"+ "═"*(maxLen+2) + "╬" + "═"*(8) +  "╬" + "═"*(8)+ "║" + "\n")
+
+        i+=1
+
+    file.write("╚" + "═"*(maxLen +2) + "╩" + "═"*(8) +"╩" + "═"*(8) + "╝" + "\n")
+    file.close()
+
+def addtoteam():
+    teams = getTeams()
+    for team in teams:
+        teams[team]["ghintcomplete"] = False
+    jfile = open(jteams, 'w')
+    json.dump(teams, jfile)
+    jfile.close()
+
---
+++ bot.py
@@ -0,0 +1,274 @@
+# bot.py
+
+import os
+import json
+
+import discord
+from discord.ext import commands    # gets the bot commands archive
+from discord.utils import get       # gets the finding functions
+from discord.ext import commands
+
+from dotenv import load_dotenv
+
+import Data
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+messages = json.load(open("messages.json"))
+lists = json.load(open("lists.json"))
+
+intents = discord.Intents.default()
+intents.members = True # Subscribe to the privileged members intent.
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# bot.remove_command('help')
+
+## EVENTS
+@bot.event # Connection Information
+async def on_ready():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(messages["bootMessage"].format(bot.user, guild.name, guild.id))
+
+    members = '\n - '.join([member.name for member in guild.members])
+    roles = '\n - '.join([channel.name for channel in guild.channels])
+    print(f'Guild Members:\n - {members}')
+    print(f'Guild Roles:\n - {roles}')
+
+    print(f'WelcomeBot Is Online!')
+
+@bot.event  # Sending the Informational DM to the New User!
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(messages["welcomeMessage"].format(member.name))
+
+## CORE BLOCK COMMANDS
+@bot.event
+async def on_raw_message_edit(msg):
+    channel = await bot.fetch_channel(msg.data["channel_id"])
+    msg_full = await channel.fetch_message(msg.message_id)
+    await bot.process_commands(msg_full)
+
+# Checking Bot Status
+@bot.command(name='WBstatus', help="Gives Bots Status",pass_context=True, commands_heading="Debug")
+async def WBstatus(ctx): # !WBstatus[Member] [Role]
+    # Checks if Responsive
+    await ctx.send(messages["statusMessage"])
+
+# Checking Bot Ping
+@bot.command(name='WBping', help="Gives Bots Ping",pass_context=True, commands_heading="Debug")
+async def WBping(ctx):
+    await ctx.send('Welcome Bots Latency: {0}'.format(round(bot.latency, 2)))
+
+# Giving a member a role!
+@bot.command(name='WBgive_role', help="Gives a member a specific role/assigns team",pass_context=True, commands_heading="Moderation")
+async def WBgive_role(ctx, user: discord.Member, role: discord.Role): # !giverole [Member] [Role]
+    if discord.utils.get(ctx.guild.roles, name="@Sticker People") not in ctx.message.author.roles:
+        await ctx.send(messages["noAccess"])
+        return
+    
+    # Adds the Role to the user 
+    await user.add_roles(role)
+
+    # Sends confirmation message
+    await ctx.send(f"The User {ctx.author.name}, {user.name} has been giving a role called: {role.name}") ##FOR DEBUGGING REMOVE THIS LINE
+
+# Creating A Private Text Channel 
+@bot.command(name='WBcreate_Pchannel', help="Creates the private team channel", commands_heading="Moderation")
+@commands.has_permissions(manage_channels=True, manage_roles=True)
+async def WBcreate_Pchannel(ctx, *, ChannelName_Role=''):
+    if ChannelName_Role == '':
+        await ctx.send("Please list a channel name to create.")
+    guild = ctx.guild
+    role =  ChannelName_Role
+    autorize_role = await guild.create_role(name=role)
+    overwrites = {
+        # normies just joining cant see it!
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        # guild.me is the bot so it keeps the bot from taking commands in new channel
+        guild.me: discord.PermissionOverwrite(read_messages=True), 
+        guild.me: discord.PermissionOverwrite(send_messages=True), 
+        # People with a specific role can see it
+        #ctx.author : discord.PermissionOverwrite(read_messages=True),
+        autorize_role: discord.PermissionOverwrite(read_messages=True),
+        autorize_role: discord.PermissionOverwrite(send_messages=True)
+    }
+    await guild.create_text_channel(ChannelName_Role, overwrites=overwrites)
+    # await ctx.author.add_roles(autorize_role)
+
+##  Advanced Command Block Sets
+
+# Creating the team and gets member to join the team with a command 
+@bot.command(help="Create a new team!", pass_context=True, commands_heading="Hunt Commands")
+async def WBcreateteam(ctx,*,role_name=''):
+    if role_name == '':
+        await ctx.send("Please list a team name to create.")
+        return
+    role_name = "".join([x for x in role_name if x.isascii()]).lower()
+    # Getting the Command Users ID
+    author = ctx.author 
+    guild = ctx.guild               # server name 
+
+    # check if that role already exists
+    check_for_duplicate = get(ctx.message.guild.roles, name=role_name)
+
+    if check_for_duplicate == None: # if the role doesn't exist
+        # create the role and store role id in authorize role!
+        authorize_role  = await guild.create_role(name=role_name, colour=discord.Colour(0x0000FF))
+
+        sPeople =  discord.utils.get(guild.roles, name="@Sticker People")
+
+        # Overwrites is a list of commands to send to the guild to change permissions
+        overwrites = {
+        # normies just joining cant see it!
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        # guild.me is the bot so it keeps the bot from taking commands in new channel
+        guild.me: discord.PermissionOverwrite(read_messages=True), 
+        guild.me: discord.PermissionOverwrite(send_messages=True), 
+        # People with a specific role can see it
+        authorize_role: discord.PermissionOverwrite(read_messages=True)
+        }
+
+        newChan = await guild.create_text_channel(role_name, overwrites=overwrites)
+
+        await newChan.set_permissions(authorize_role, send_messages=True, read_messages = True)
+        await newChan.set_permissions(sPeople, send_messages=True, read_messages = True)
+
+        # Adding Team Creator to Role and Team chat!
+        
+        await author.add_roles(authorize_role) # adds the role to the user
+
+        # Dming the New Chat with info!
+
+        descript = (messages["newTeamMessage"])
+        embedmessage=discord.Embed(title=f"Welcome to the {role_name}'s Teams DM", description=descript, color=0x5bcdee)
+        embedmessage.set_footer(text="Good Luck!")
+        
+        await newChan.send(embed=embedmessage)  # Sends DM to Channel
+
+        Data.addTeam(role_name)
+
+        team_channel_id = newChan.id
+        await ctx.send("Your shiny new team awaits you! <#{}>".format(team_channel_id))
+        
+    else: # TEAM ALREADY EXISTS BREAK OFF!
+        await ctx.send(f"This team/role already exists try another name!")  # Checks for error!
+
+# Joining already created team Command
+@bot.command(help="Join a preexisting team", pass_context=True, commands_heading="Hunt Commands")
+async def WBjointeam(ctx, roleName=''):
+    if roleName == '':
+        await ctx.send("Please list a team name to join.")
+        return
+
+    forbiddenRoles = lists["forbidden"]
+    if roleName in forbiddenRoles:
+        await ctx.send("Don't worry, we're smarter than that.")
+        return
+
+    author = ctx.message.author # grabs the member id that used command
+    if any(role.name == roleName for role in author.roles):
+        await ctx.send("You already have a team! Contact a Sticker Person.")
+
+    else:
+        roleToAdd = discord.utils.get(ctx.guild.roles, name=roleName)
+        if(roleToAdd == None):
+            await ctx.send("This is not a role!")
+            
+        else:
+            await ctx.author.add_roles(roleToAdd)
+            await ctx.send("Role has been added!")
+
+@bot.command(help="Submit a code you have found: <name> <key>", pass_context=True, commands_heading="Hunt Commands")
+async def WBcode(ctx, codeword='', key=''):
+    team = ctx.message.channel
+
+    if team.name not in Data.getTeams():
+        await ctx.send(messages["validMessage"])
+        return
+
+    if codeword == '' or key == '':
+        await ctx.send("Invalid input, make sure you input is in format `!WBcode <sticker> <authCode>")
+        return
+
+    await ctx.send(Data.addSticker(ctx.channel.name, codeword, key))
+    
+@bot.command(help="Displays your team's score", pass_context=True, commands_heading="Hunt Commands")
+async def WBscore(ctx):
+    await ctx.send(Data.printScoreAndCount(ctx.message.channel.name))
+
+@bot.command(help="The worse help function", pass_context=True, commands_heading="Help")
+async def WBhelp(ctx):
+    await ctx.send(messages["helpMessage"])
+
+@bot.command(help="Formats the scoreboard with the most up to date information", pass_context=True, commands_heading="Moderation")
+async def WBscoreboard(ctx):
+    if ctx.message.channel.id not in [950575250112909452, 852725464518361108]:
+        await ctx.send("Looks like this isn't the right channel for that, try again in the correct location!")
+        return
+
+    Data.scoreBoard()
+    file = open("data.txt", "r", encoding="utf-8")
+    
+    scores = "```" + file.read() + "```"
+    
+    file.close()
+
+    embedded = discord.Embed(title = "LeaderBoard", description=scores, color = 0xF1C40F)
+    await ctx.send(embed=embedded)
+
+@bot.command(help="Removes a team from the server and database", pass_context=True, commands_heading="Moderation")
+async def WBremoveteam(ctx, team=''):
+    if discord.utils.get(ctx.guild.roles, name="@Sticker People") not in ctx.message.author.roles:
+        await ctx.send(messages["noAccess"])
+    
+    elif team == '':
+        await ctx.send("Please list a team name to remove.")
+    
+    else:
+        team = team.lower()
+        await ctx.send(Data.removeTeam(team))
+        channel = discord.utils.get(ctx.guild.channels, name=team)
+        await channel.delete()
+        role = discord.utils.get(ctx.guild.roles, name=team)
+        await role.delete()
+
+@bot.command(help="View the full list of teams", pass_context=True, commands_heading="Moderation")
+async def WBteamlist(ctx):
+    if discord.utils.get(ctx.guild.roles, name="@Sticker People") not in ctx.message.author.roles:
+        await ctx.send(messages["noAccess"])
+    
+    else:
+        await ctx.send(Data.printTeams())
+
+@bot.command(help='Request a hint, as well as viewing your current hints.',pass_context=True, commands_heading="Hunt Commands")
+async def WBhint(ctx):
+    team = ctx.message.channel
+
+    if team.name not in Data.getTeams():
+        await ctx.send(messages["validMessage"])
+        return
+
+    await ctx.send(Data.getHint(team.name))
+
+@bot.command(help="Makes it die and dumps all its files.", pass_context=True)
+async def keep_inventory(ctx):
+    if discord.utils.get(ctx.guild.roles, name="@Sticker People") not in ctx.message.author.roles:
+        await ctx.send(messages["noAccess"])
+        return
+    await ctx.send("Goodbye for now. <3")
+    channel = discord.utils.get(ctx.guild.channels, name="backup")
+    for f in lists["files"]:
+        await channel.send(file=discord.File(f))
+    exit("All done!")
+
+@bot.command(help="Makes it die.", pass_context=True)
+async def kill(ctx):
+    if discord.utils.get(ctx.guild.roles, name="@Sticker People") not in ctx.message.author.roles:
+        await ctx.send(messages["noAccess"])
+        return
+    await ctx.send("Goodbye for now. <3")
+    exit("All done!")
+
+bot.run(TOKEN) # end of bot
